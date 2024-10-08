d5a77f9705a3f84f228bc02232a6b5a7764ef25b
spridernp <pathekarnishita@gmail.com>
2024-10-08 20:26:19 +0530

    Commit_#5

--- src/main/java/com/dependencyresolver/service/DependencyResolver.java
+++ src/main/java/com/dependencyresolver/service/DependencyResolver.java
@@ -4,8 +4,6 @@ import com.dependencyresolver.dto.NotificationResponse;
 import com.dependencyresolver.dto.ResolvedGroupDTO;
 import com.dependencyresolver.entity.Notification;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
@@ -14,7 +12,6 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 public class DependencyResolver {
-    private static final Logger logger = LoggerFactory.getLogger(DependencyResolver.class);
     private final NotificationReceiverService notificationReceiverService;
     @Autowired
     public DependencyResolver(NotificationReceiverService notificationReceiverService) {
@@ -27,19 +24,19 @@ public class DependencyResolver {
         groups.forEach((groupName, groupFeedIds) -> {
 
             List<Notification> latestNotifications=notificationReceiverService.findLatestNotificationsByCobAndFeedGroup(cob,groupFeedIds);
-            logger.info("List of Notifications in group {}:{}",groupName,latestNotifications);
+            log.info("List of Notifications in group {}:{}", groupName, latestNotifications);
             Set<Long> notificationFeedIds = latestNotifications.stream()
                     .map(notification -> notification.getFeed().getId())
                     .collect(Collectors.toSet());
             boolean allFeedIdsPresent = groupFeedIds.stream()
                     .allMatch(notificationFeedIds::contains);
             if (allFeedIdsPresent) {
-                logger.info("All groupFeedIds are present in the latestNotifications for group {}.", groupName);
+                log.info("All groupFeedIds are present in the latestNotifications for group {}.", groupName);
                 // Convert Notifications to NotificationResponse
                 NotificationResponse[] notificationResponses = latestNotifications.stream()
-                        .map(notification -> new NotificationResponse(notification.getFeed().getFeedName(), notification.getMessage(), notification.getAddress()))
+                        .map(notification -> new NotificationResponse(notification.getFeed().getFeedName(),
+                                notification.getMessage(), notification.getAddress()))
                         .toArray(NotificationResponse[]::new);
-
                 // Build the Response
                 ResolvedGroupDTO resolvedGroupDTO = ResolvedGroupDTO.builder()
                         .cob(cob)
@@ -48,10 +45,10 @@ public class DependencyResolver {
                         .build();
                 resolvedGroupDTOS.add(resolvedGroupDTO);
                 // Log the Response
-                logger.info("Resolved Group Response: {}", resolvedGroupDTO);
+                log.info("Resolved Group Response: {}", resolvedGroupDTO);
 
             } else {
-                logger.warn("Not all groupFeedIds are present in the latestNotifications for group {}.", groupName);
+                log.warn("Not all groupFeedIds are present in the latestNotifications for group {}.", groupName);
             }
         });
         return resolvedGroupDTOS;
